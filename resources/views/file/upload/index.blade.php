17e748e256b837907ecd08429c1260002da863e7
zamanshahed <shaheduz.zaman@northsouth.edu>
2022-01-25 15:38:20 +0600

    UI: upload page finalized, waiting for backend

--- resources/views/file/upload/index.blade.php
+++ resources/views/file/upload/index.blade.php
@@ -4,14 +4,38 @@
             {{ __('Upload File') }}
         </h2>
 
-        
-        
+
+
 
     </x-slot>
 
-    <div class="py-12">
-        <div class="container">
-            
+    <div class="container" style="margin-top: 40px; width: 500px">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 style="text-align: center">
+                            Upload File
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        {{-- form input starts --}}
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Name </label>
+                                <input type="text" class="form-control" placeholder="Enter a name">
+                                
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Choose a File to upload</label>
+                                <input type="file" class="form-control" >
+                            </div>
+                            
+                            <button type="submit" class="btn btn-outline-primary">Upload</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
