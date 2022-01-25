5903f76f36d70c521cabdc7b24e41b9d9d6aa668
zamanshahed <shaheduz.zaman@northsouth.edu>
2022-01-25 15:01:18 +0600

    ui tweaks

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,15 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        <b style="float: right">Total Users: <span class="badge bg-primary">{{ count($users) }}</span> </b>
+
+        <b style="float: right">
+            Total Users: <span class="badge bg-primary">{{ count($users) }}</span> 
+            Total Files: <span class="badge bg-primary">{{ count($users) }}</span> 
+            Total Groups: <span class="badge bg-primary">{{ count($users) }}</span>
+        </b>
         <br>
+        
+
     </x-slot>
 
     <div class="py-12">
